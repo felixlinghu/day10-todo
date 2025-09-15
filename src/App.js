c4c8fdc8a9fd30c6641720583c368dfe9775818d
Felix Linghu <felix.linghu@oocl.com>
2025-09-15 10:49:56 +0800

    feat:add error handling with ErrorPage component in routing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css"
 import {todoReducer} from "./reducers/TodoReducer";
 import {TodoContext as TodoContext1} from "./contexts/TodoContext";
 import {MultipleTodo} from "./components/MultipleTodo";
-import {createBrowserRouter, NavLink, Outlet, RouterProvider} from "react-router";
+import {createBrowserRouter, NavLink, Outlet, RouterProvider, useRouteError} from "react-router";
 
 function DefalutLayout() {
   return <div>
@@ -22,35 +22,52 @@ function DefalutLayout() {
   </div>
 }
 
+function ErrorPage() {
+  const error = useRouteError();
+  return <div>
+    {error.status === 404 ?
+        <div className={"not-found"}>
+          <h1>
+        <span>
+          Try
+        </span>
+          </h1>
+        </div> : <div>{JSON.stringify("error")}
+        </div>
+    }
+  </div>
+}
+
 const browserRouter = createBrowserRouter(
     [
-{
-      path:"/",
-      element:<DefalutLayout/>,
-  children:[
-    {
-      path:"/",
-      element:<MultipleTodo/>
-    }
-  ]
-    }
+      {
+        path: "/",
+        element: <DefalutLayout/>,
+        errorElement: <ErrorPage/>,
+        children: [
+          {
+            path: "/",
+            element: <MultipleTodo/>
+          }
+        ]
+      }
     ]
 );
 export const initState = [
-    {id: 1, text: "This is the first thing I need to do", done: false},
-    {id: 2, text: "This is the second thing I need to do", done: false},
+  {id: 1, text: "This is the first thing I need to do", done: false},
+  {id: 2, text: "This is the second thing I need to do", done: false},
 ];
 
 function App() {
-    const [state, dispatch] = useReducer(todoReducer, initState);
-    return (
-        <div>
+  const [state, dispatch] = useReducer(todoReducer, initState);
+  return (
+      <div>
 
-            <TodoContext1 value={{state, dispatch}}>
-              <RouterProvider router={browserRouter}></RouterProvider>
-            </TodoContext1>
-        </div>
-    );
+        <TodoContext1 value={{state, dispatch}}>
+          <RouterProvider router={browserRouter}></RouterProvider>
+        </TodoContext1>
+      </div>
+  );
 }
 
 export default App;
