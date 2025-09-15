94f1dd19b808970c1cd271e02252809b0d08e149
Felix Linghu <felix.linghu@oocl.com>
2025-09-15 10:39:47 +0800

    feat:add routing with react-router and create default layout

--- src/App.js
+++ src/App.js
@@ -3,7 +3,39 @@ import "./App.css"
 import {todoReducer} from "./reducers/TodoReducer";
 import {TodoContext as TodoContext1} from "./contexts/TodoContext";
 import {MultipleTodo} from "./components/MultipleTodo";
+import {createBrowserRouter, NavLink, Outlet, RouterProvider} from "react-router";
 
+function DefalutLayout() {
+  return <div>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to={"/"}>home</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <Outlet/>
+    </main>
+  </div>
+}
+
+const browserRouter = createBrowserRouter(
+    [
+{
+      path:"/",
+      element:<DefalutLayout/>,
+  children:[
+    {
+      path:"/",
+      element:<MultipleTodo/>
+    }
+  ]
+    }
+    ]
+);
 export const initState = [
     {id: 1, text: "This is the first thing I need to do", done: false},
     {id: 2, text: "This is the second thing I need to do", done: false},
@@ -13,8 +45,9 @@ function App() {
     const [state, dispatch] = useReducer(todoReducer, initState);
     return (
         <div>
+
             <TodoContext1 value={{state, dispatch}}>
-                <MultipleTodo/>
+              <RouterProvider router={browserRouter}></RouterProvider>
             </TodoContext1>
         </div>
     );
